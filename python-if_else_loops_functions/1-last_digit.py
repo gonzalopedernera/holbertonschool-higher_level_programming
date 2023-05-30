26e53f0bcb53b88051609ed23f999d2a74068a56
gonzalopedernera <gonzalopeder2@gmail.com>
2023-05-30 14:28:23 -0700

    Task 1 if statement error correction

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -12,7 +12,7 @@ if (ld == 0):
     print("and is 0", end="")
 elif (ld > 5):
     print("and is greater than 5 ", end="")
-elif (ld < 6 & ld != 0):
+elif (ld < 6 and ld != 0):
     print("and is less than 6 and not 0", end="")
 
 print("")
