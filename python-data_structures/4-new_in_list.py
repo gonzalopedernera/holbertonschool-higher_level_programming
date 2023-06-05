1fa13a23550210d7701652429c768e4cb0d4dfb5
root <gonzalopeder2@gmail.com>
2023-06-05 11:08:40 -0700

    Task 4 completed, working and Pycodestyle corrected

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list:
-        new_list = my_list.copy()
-        new_list.pop(idx)
-        new_list.insert(idx, element)
-        return (new_list)
+        if idx >= 0 and idx < len(my_list):
+            new_list = my_list.copy()
+            new_list.pop(idx)
+            new_list.insert(idx, element)
+            return (new_list)
