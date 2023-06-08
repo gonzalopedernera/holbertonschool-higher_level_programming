ae3ef5befc6cf5e53667949d512500edb3b192f8
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-08 10:26:51 -0700

    Task 12 completed, working and Pycodestyle corrected

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    res = 0
     if type(roman_string) == str and roman_string is not None:
-        for i in range(len(roman_string)):
+        res = 0
+        for i in range(0, len(roman_string)):
             currval = values.get(roman_string[i])
             res += currval
             if i > 0:
                 prevval = values.get(roman_string[i - 1])
                 if prevval < currval:
-                    res -= prevval
+                    res -= prevval * 2
         return res
     else:
         return 0
