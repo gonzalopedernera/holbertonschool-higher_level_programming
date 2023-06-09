a9cec90825b238f23e9a965424c5a2d60c8cf269
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-09 04:30:46 -0700

    Task 0 completed, working and Pycodestyle corrected

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for i in range(0, x):
-            print("{:d}".format(my_list[i]), end=" ")
+            print("{}".format(my_list[i]), end=" ")
             nb += 1
         print()
         return nb
