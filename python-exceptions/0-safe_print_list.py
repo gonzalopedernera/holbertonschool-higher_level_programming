8b56d9d547eeb5ed3ef3acb578ae65226b5f005a
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-09 04:33:42 -0700

    Task 0 error correction

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,12 +3,11 @@
 def safe_print_list(my_list=[], x=0):
     nb = 0
 
-    try:
-        for i in range(0, x):
+    for i in range(0, x):
+        try:
             print("{}".format(my_list[i]), end=" ")
-            nb += 1
-        print()
-        return nb
-    except IndexError:
-        print()
-        return nb
+        except IndexError:
+            break
+        nb += 1
+    print()
+    return nb
