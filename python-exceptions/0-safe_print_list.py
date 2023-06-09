4d913fdf591d18e0126febd545a273b1265fbffe
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-09 04:43:14 -0700

    Task 0 error correction

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     nb = 0
-    while nb <  x:
+    while nb < x:
         try:
             print("{}".format(my_list[nb]), end="")
         except IndexError:
