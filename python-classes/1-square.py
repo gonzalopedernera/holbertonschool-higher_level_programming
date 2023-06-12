c2b4497f4bbb12b23ac562a9a1abcc3c5664ed0d
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-12 07:45:19 -0700

    Task 1 added documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
+""" Defines 'Square' class with attribute size """
+
+
 class Square:
+    """ Initialization of squares
+
+    Args:
+        size (int): size of the new square """
     def __init__(self, size):
         self.__size = size
