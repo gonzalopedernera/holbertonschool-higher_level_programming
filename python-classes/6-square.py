4ae628ff34ab18a9f7b59a07aa2b59148c80fc2d
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-13 11:29:53 -0700

    Task 6 exception correction

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -15,7 +15,10 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        if type(position) is not tuple or position[0] < 0 or position[1] < 0:
+        if type(position) is not tuple or len(position) != 2 \
+                or type(position[0]) is not int \
+                or type(position[1]) is not int \
+                or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
