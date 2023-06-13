64aab2f53a3248d34b5b529968739c647d902dd6
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-13 10:49:37 -0700

    Task 6 completed, working and Pycodestyle corrected

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -58,7 +58,13 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            if self.__position[1] > 0:
+                for k in range(0, self.__position[1]):
+                    print()
             for i in range(0, self.__size):
+                if self.__position[0] > 0:
+                    for k in range(0, self.__position[0]):
+                        print(" ", end="")
                 for j in range(0, self.__size):
                     if j != self.__size - 1:
                         print("#", end="")
