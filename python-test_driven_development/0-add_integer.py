0fce06888c5125f3da66eddf96ffacaf8f0704b5
root <gonzalopeder2@gmail.com>
2023-06-14 11:08:20 -0700

    Task 0 documented

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """ Adds two digits and converts the result to ints
+    Args:
+        a: a number
+        b: a number
+
+    Returns:
+        Addition of a and b converted to an int
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
