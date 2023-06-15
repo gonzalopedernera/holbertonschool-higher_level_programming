acac0eb1f7085ff521c5a94ea20a8a07f5069eb9
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-15 01:29:05 -0700

    Task 3 completed, working and pycodestyle corrected

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" prints My name is <first name> <last name> """
+
+
+def say_my_name(first_name, last_name=""):
+    """ prints My name is <first name> <last name>
+    Args:
+        first_name: first name string
+        last_name: last_name string
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
