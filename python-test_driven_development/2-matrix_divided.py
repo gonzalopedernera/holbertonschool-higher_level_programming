4d3c5925d8ba082af21b1b6248750addc660f386
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-15 01:03:33 -0700

    Task 1 correction and documentation

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
+""" Divides all elements on a matrix by a specified divisor """
+
+
 def matrix_divided(matrix, div):
-    if type(div) not in [int, float]:
-        raise TypeError("div must be a number")
+    """ Divides all elements on a matrix by a specified divisor
+    Args:
+        matrix: a matrix of floats/ints
+        div: Divisor for the division
 
-    listlen = len(matrix[0])
-    if any(len(row) != listlen for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+    Returns:
+        a new matrix with all the results
+    """
+    if len(matrix) != 0:
+        if type(div) not in [int, float]:
+            raise TypeError("div must be a number")
 
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
+        listlen = len(matrix[0])
+        if any(len(row) != listlen for row in matrix):
+            raise TypeError("Each row of the matrix must have the same size")
 
-    msg = "matrix must be a matrix (list of lists) of integers/floats"
-    for row in matrix:
-        for i in row:
-            if type(i) not in [int, float]:
-                raise TypeError(msg)
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
 
-    newmatrix = []
-    for j in matrix:
-        newmatrix.append(list(map(lambda x: round(x / div, 2), j)))
-    return newmatrix
+        msg = "matrix must be a matrix (list of lists) of integers/floats"
+        for row in matrix:
+            for i in row:
+                if type(i) not in [int, float]:
+                    raise TypeError(msg)
+
+        newmatrix = []
+        for j in matrix:
+            newmatrix.append(list(map(lambda x: round(x / div, 2), j)))
+        return newmatrix
+    else:
+        return matrix
