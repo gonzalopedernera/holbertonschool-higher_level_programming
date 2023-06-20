637c42fad1f5838265d084dcd3bb5a16abd1e8e0
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-20 04:04:49 -0700

    Task 0 completed, working and Pycodestyle corrected

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"gets all the available attributes and methods of an object"
+
+
 def lookup(obj):
+    """Returns a list with available attributes and methods of an object
+    Args:
+        object: an object
+    """
     return list(dir(obj))
