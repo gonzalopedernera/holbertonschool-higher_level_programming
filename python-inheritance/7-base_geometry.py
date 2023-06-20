a15b19444ff2dbf9e724777069038ea5a5cacc2a
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-20 06:37:51 -0700

    Task 7 completed, working and Pycodestyle corrected

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""New empty class"""
+
+
+class BaseGeometry:
+    """Function placeholder with exception message"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
