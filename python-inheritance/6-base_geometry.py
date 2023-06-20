e8511a3c370f903f8e0e1db2213c65fbbf676f3a
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-20 06:13:10 -0700

    Task 6 completed, working and Pycodestyle corrected

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""New empty class"""
+
+
+class BaseGeometry:
+    """Function placeholder with exception message"""
+    def area(self):
+        raise Exception("area() is not implemented")
