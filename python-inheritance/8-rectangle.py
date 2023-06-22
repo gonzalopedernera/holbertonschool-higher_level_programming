9df26b9c087733ed774bfec3b2fcb8ffe167a5be
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-22 11:10:26 -0700

    Task 8 completed, working and Pycodestyle corrected

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Class Rectangle inheriting from BaseGeometry"""
+
+
+class Rectangle(BaseGeometry):
+    """Initialization of a new class object
+    Args:
+        width (int): width of the new Rectangle
+        height (int): height of the new Rectangle
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
