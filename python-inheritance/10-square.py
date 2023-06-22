3bfea2e47a05db49cc99f697ab17fd6cae0b1394
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-22 11:34:23 -0700

    Task 10 completed, working and Pycodestyle corrected

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Import Rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square object initialization
+        Args:
+            size(int): size of the square"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    """Return the area of a square object"""
+    def area(self):
+        return self.__size * self.__size
