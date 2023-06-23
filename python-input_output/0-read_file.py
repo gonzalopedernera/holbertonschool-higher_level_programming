fe1fe0fdc94d6ecbe26b02c5b717bc3249a6be41
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-23 06:30:58 -0700

    Task 0 completed, working and Pycodestyle corrected

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     """
     with open(filename, encoding="utf-8") as f:
         reading = f.read()
-        print("{}".format(reading))
+        print("{}".format(reading), end="")
     f.closed
