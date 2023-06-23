0f636340afa92b291f5c47ea0def0ea19ba5ed97
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-23 07:33:20 -0700

    Task 3 completed, working and Pycodestyle corrected:

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""Import Json module"""
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)
+    Args:
+        my_obj: object to convert
+    """
+    return json.dumps(my_obj)
