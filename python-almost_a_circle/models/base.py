c78e0ba203477a3a201292f9e45c9a73d618a1a4
root <gonzalopeder2@gmail.com>
2023-06-27 06:37:33 -0700

    Task 1 completed, working and Pycodestyle corrected

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,11 +8,11 @@ class Base:
     Args:
         id: new object id
     """
-    _nb__objects = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is None:
+        if id is not None:
+            self.id = id
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        else:
-            self.id = id
