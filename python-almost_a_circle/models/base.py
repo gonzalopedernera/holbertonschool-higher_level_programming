84d93f875b38d4e6947869af7e899b246fe02b04
gonzalopedernera <gonzalopeder2@gmail.com>
2023-06-29 12:21:28 -0700

    Task 15 completed, working and Pycodestyle corrected

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Importing JSON module"""
+import json
+
 """New class Base"""
 
 
@@ -16,3 +19,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries
+        Args:
+            list_directories: a list of dictionaries
+        """
+        return json.dumps(list_dictionaries)
